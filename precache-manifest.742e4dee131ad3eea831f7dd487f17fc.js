543988f300f6bb2c1c9117b8456564c1baec8bc0
DiegoSoler <37308832+DiegoGSS@users.noreply.github.com>
2020-12-26 21:52:14 -0300

    Updates

---
+++ precache-manifest.742e4dee131ad3eea831f7dd487f17fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c42392e7f328a4cf8b4a9dfd5bbe6fd",
+    "url": "/iegod-project/index.html"
+  },
+  {
+    "revision": "a6b2ebfb0e582fb1ea8f",
+    "url": "/iegod-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6916cccd387137412960",
+    "url": "/iegod-project/static/js/2.611edbef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/iegod-project/static/js/2.611edbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b2ebfb0e582fb1ea8f",
+    "url": "/iegod-project/static/js/main.08585193.chunk.js"
+  },
+  {
+    "revision": "6ee96dd60d6ad047be35",
+    "url": "/iegod-project/static/js/runtime-main.69395dc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d7951a21fbd330d991f60b430c44478.js"
+  "/iegod-project/precache-manifest.742e4dee131ad3eea831f7dd487f17fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iegod-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
